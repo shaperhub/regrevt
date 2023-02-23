f48aef2def55f6fad6fc65840048bc2f9f3dccf0
Leo Nwoke <leonnwoke@gmail.com>
2023-02-23 13:32:14 -0600

    Added preload to AOS

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import Banner from './Banner'
 import Meta from './Meta'
+import Navbar from './Navbar'
 
 
 const Layout = ({children}) => {
   return (
     <>
       <Meta />
+      <Navbar />
+      <Banner />
+      <Footer />
     </>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -12,8 +12,8 @@ export default function Home() {
         
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" />
         
-        <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
-        <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+        <link rel="preload" as="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+        <link rel="preload" as="script" href="https://unpkg.com/aos@next/dist/aos.js" />
         <script>
     	    AOS.init();
         </script>
