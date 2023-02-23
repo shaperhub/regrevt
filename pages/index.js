3bb13a6bf8e3fa069c2260478157d679b2e61598
Leo Nwoke <leonnwoke@gmail.com>
2023-02-23 13:39:58 -0600

    Remove preload from AOS js link

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,8 @@ export default function Home() {
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" />
         
         <link rel="preload" as="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
-        <link rel="preload" as="script" href="https://unpkg.com/aos@next/dist/aos.js" />
+        {/* <link rel="preload" as="script" href="https://unpkg.com/aos@next/dist/aos.js" /> */}
+        <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
         <script>
     	    AOS.init();
         </script>
